733c7c6181b2ad886e527042101cf1447aa47909
ghhyc <yenth602@gmail.com>
2021-12-16 15:50:46 -0800

    add completed app file

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+
+
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars_index=mars)
+
+
+@app.route("/scrape")
+def scraper():
+    mars = mongo.db.mars
+    mars_data = scrape.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
